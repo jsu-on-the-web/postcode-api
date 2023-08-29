4444f265818ab3dbd2350cbfb0549864f0a484be
Janet Suryadi <janet@suryadi.id.au>
2023-08-29 20:46:40 +1000

    Adding docs to a repository method

--- src/main/java/com/jsu/postcodeapi/Postcode/PostcodeRepository.java
+++ src/main/java/com/jsu/postcodeapi/Postcode/PostcodeRepository.java
@@ -6,6 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostcodeRepository extends JpaRepository<Postcode, Long> {
 
+    
+    /**
+     * Sends a query to the database to check if the postcode is present in the database
+     * @param postcode - the postcode to check
+     * @return Whether the postcode exists on the database or not
+     */
     @Query("SELECT CASE WHEN p.postcode = ? THEN 1 ELSE 0 END FROM Postcode p")
     Boolean exists(Integer postcode);
 
